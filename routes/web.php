4a88cbfca34a6c76ae4b6b2a2094278d6726f5f4
Cosimo-Colucci <ccoluccilavoro@gmail.com>
2023-07-27 17:20:58 +0200

    added route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PageController as AdminPageController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[AdminPageController::class,'index'])->name('admin.shores.index');
